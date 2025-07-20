daede84302317853f1dd6b29169a0dde0a252e62
h-ars <harsh.raghukul@gmail.com>
2025-07-20 22:14:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { db } from './firebase';
+
+const App = () => {
+  
+  return (
+    <h1 class="text-3xl font-bold underline text-blue-600">
+      Hello world!
+    </h1>
+  );
+}
+ 
+export default App;
